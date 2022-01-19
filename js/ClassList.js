a76c8bfe29e598d5505b34a9079324e76f2aeba9
Anton <ababkov.ao@yandex.ru>
2022-01-19 19:02:22 +0300

    закончил урок по  ClassList

--- js/ClassList.js
+++ js/ClassList.js
@@ -22,3 +22,32 @@ btns[1].classList.add('red');            //Добавили класс
 if (btns[1].classList.contains('red')) { //Метод contains объекта classList проверяет наличие CSS класса элемента.
     console.log('contains сработал');
 }
+
+
+btns[0].addEventListener('click', () => { //Пример события,меняющего классы у элементов.  
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red'); //Вместо add и remove можно использовать toggle.
+        console.log('Добавили класс red');
+    } else {
+        btns[1].classList.remove('red');
+        console.log('Удалили класс red');
+    }
+});
+
+console.log(btns[0].className); //Так можно посмотреть классы объекта. УСТАРЕЛО
+
+
+/* Делегирование событий - это передача событий от родителя к потомку, таким образом событие можно назначать только родительскому элементу, не вешая события на каждый из элементов. */
+
+const wrapper = document.querySelector('.btn-block'); //Создали переменную с родительским элементом, на которую будет подвешанно событие.
+wrapper.addEventListener('click', (event) => {
+    console.dir(event.target); //Так можно вывести в консоль объект со всеми свойствами.
+    if (event.target && event.target.matches("button.red")) { //метод matches полезен когда на странице много похожих элементов и нужен какой-то конкретный. 
+        console.log('hello');
+    }
+});
+
+/* При делигировании событий, все новые элементы будут с уже назначенными событиями. */
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
